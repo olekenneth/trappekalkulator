23a4da4080f7ba41cd3124fb3d8791d6645d6ab2
Ole-Kenneth Bratholt <ok@rait.no>
2024-05-05 22:48:41 +0200

    Add eslint

---
+++ eslint.config.js
@@ -0,0 +1,25 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+
+export default [
+  { ignores: ["tailwind.config.mjs"] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReactConfig,
+  {
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "@typescript-eslint/triple-slash-reference": "off",
+    },
+  },
+  {
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+];
